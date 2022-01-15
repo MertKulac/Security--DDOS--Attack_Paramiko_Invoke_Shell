40e4f87792dd6848aad67969a9183b6bfb0b41f1
Mert Kulac <96883175+MertKulac@users.noreply.github.com>
2022-01-15 17:26:32 +0300

    Update ddos_monitoring.py

--- ddos_monitoring.py
+++ ddos_monitoring.py
@@ -1,14 +1,3 @@
-import paramiko
-import re
-import os
-from termcolor import colored
-from queue import Queue
-import time, smtplib
-import pandas as pd
-from SIFRE import username, password
-import schedule
-import time
-import ipaddress
 
 def SendMailwAttachment_(List_, Ek):
     from email import encoders
