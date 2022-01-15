cbb73aa7a9e70a87f50d80895eb559ce7528c3c6
Mert Kulac <96883175+MertKulac@users.noreply.github.com>
2022-01-15 17:38:16 +0300

    Update ddos_monitoring.py

--- ddos_monitoring.py
+++ ddos_monitoring.py
@@ -1,16 +1,3 @@
-
-def XLSExport(Rows, SheetName, FileName):
-    from openpyxl import Workbook
-    wb = Workbook()
-
-    ws = wb.active
-    ws.title = SheetName
-    # ws = wb.create_sheet(SheetName)
-    for x in Rows:
-        ws.append(x)
-
-    wb.save(FileName)
-
 def ddos_update():
 
     ExcelExport = [["CI", "IP", "Global Network", "Global Next Hop", "TAC"]]
