e410ace2c44ba38f37a46022142006056bb6b55d
Mert Kulac <96883175+MertKulac@users.noreply.github.com>
2022-01-15 17:27:56 +0300

    Update library.py

--- library.py
+++ library.py
@@ -5,7 +5,7 @@ from termcolor import colored
 from queue import Queue
 import time, smtplib
 import pandas as pd
-from SIFRE import username, password
+from PASSWORD import username, password
 import schedule
 import time
 import ipaddress
